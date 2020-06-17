813b33a883514f32681bc7277edf95947c4c4064
Amory Meltzer <Amorymeltzer@gmail.com>
2020-06-16 20:31:43 -0400

    twinkle: Update vector classes (vectorMenu -> vector-menu) for the Twinkle menu
    https://github.com/azatoth/twinkle/commit/17d664ff4c7b09e2b807edd1fc23245cc8a8fa12

--- twinkle.js
+++ twinkle.js
@@ -174,14 +174,15 @@ Twinkle.getFriendlyPref = function twinkleGetFriendlyPref(name) {
  * portlet menu types all work slightly different.
  *
  * Available navigation areas depend on the skin used.
+ * Vector:
+ *  For each option, the outer div class contains "vector-menu", the inner div class is "vector-menu-content", and the ul is "vector-menu-content-list"
+ *  "mw-panel", outer div class contains "vector-menu-portal". Existing portlets/elements: "p-logo", "p-navigation", "p-interaction", "p-tb", "p-coll-print_export"
+ *  "left-navigation", outer div class contains "vector-menu-tabs" or "vector-menu-dropdown". Existing portlets: "p-namespaces", "p-variants" (menu)
+ *  "right-navigation", outer div class contains "vector-menu-tabs" or "vector-menu-dropdown". Existing portlets: "p-views", "p-cactions" (menu), "p-search"
+ *  Special layout of p-personal portlet (part of "head") through specialized styles.
  * Monobook:
  *  "column-one", outer div class "portlet", inner div class "pBody". Existing portlets: "p-cactions", "p-personal", "p-logo", "p-navigation", "p-search", "p-interaction", "p-tb", "p-coll-print_export"
  *  Special layout of p-cactions and p-personal through specialized styles.
- * Vector:
- *  "mw-panel", outer div class "portal", inner div class "body". Existing portlets/elements: "p-logo", "p-navigation", "p-interaction", "p-tb", "p-coll-print_export"
- *  "left-navigation", outer div class "vectorTabs" or "vectorMenu", inner div class "" or "menu". Existing portlets: "p-namespaces", "p-variants" (menu)
- *  "right-navigation", outer div class "vectorTabs" or "vectorMenu", inner div class "" or "menu". Existing portlets: "p-views", "p-cactions" (menu), "p-search"
- *  Special layout of p-personal portlet (part of "head") through specialized styles.
  * Modern:
  *  "mw_contentwrapper" (top nav), outer div class "portlet", inner div class "pBody". Existing portlets or elements: "p-cactions", "mw_content"
  *  "mw_portlets" (sidebar), outer div class "portlet", inner div class "pBody". Existing portlets: "p-navigation", "p-search", "p-interaction", "p-tb", "p-coll-print_export"
@@ -226,8 +227,8 @@ Twinkle.addPortlet = function( navigation, id, text, type, nextnodeid )
 			if ( navigation !== "portal" && navigation !== "left-navigation" && navigation !== "right-navigation" ) {
 				navigation = "mw-panel";
 			}
-			outerDivClass = ( navigation === "mw-panel" ) ? "portal" : ( type === "menu" ? "vectorMenu" : "vectorTabs" );
-			innerDivClass = ( navigation === "mw-panel" ) ? "body" : ( type === "menu" ? "menu" : "" );
+			outerDivClass = 'vector-menu vector-menu-' + (navigation === 'mw-panel' ? 'portal' : type === 'menu' ? 'dropdown' : 'tabs');
+			innerDivClass = 'vector-menu-content';
 			break;
 		case "modern":
 			if ( navigation !== "mw_portlets" && navigation !== "mw_contentwrapper" ) {
@@ -246,7 +247,8 @@ Twinkle.addPortlet = function( navigation, id, text, type, nextnodeid )
 	// Build the DOM elements.
 	var outerDiv = document.createElement('nav');
 	outerDiv.setAttribute('aria-labelledby', id + '-label');
-	outerDiv.className = outerDivClass + " emptyPortlet";
+	// Vector getting vector-menu-empty FIXME TODO
+	outerDiv.className = outerDivClass + ' emptyPortlet';
 	outerDiv.id = id;
 	if ( nextnode && nextnode.parentNode === root ) {
 		root.insertBefore( outerDiv, nextnode );
@@ -254,38 +256,50 @@ Twinkle.addPortlet = function( navigation, id, text, type, nextnodeid )
 		root.appendChild( outerDiv );
 	}
 
-	var h5 = document.createElement( "h3" );
-	if ( type === "menu" ) {
-		var span = document.createElement( "span" );
-		span.appendChild( document.createTextNode( text ) );
-		h5.appendChild( span );
+	var h3 = document.createElement('h3');
+	h3.id = id + '-label';
+	var ul = document.createElement('ul');
 
-		var a = document.createElement( "a" );
-		a.href = "#";
+	if (skin === 'vector') {
+		// add invisible checkbox to keep menu open when clicked
+		// similar to the p-cactions ("More") menu
+		if (outerDivClass.indexOf('vector-menu-dropdown') !== -1) {
+			var chkbox = document.createElement('input');
+			chkbox.className = 'vectorMenuCheckbox vector-menu-checkbox'; // remove vectorMenuCheckbox after 1.35-wmf.37 goes live
+			chkbox.setAttribute('type', 'checkbox');
+			chkbox.setAttribute('aria-labelledby', id + '-label');
+			outerDiv.appendChild(chkbox);
 
-		$( a ).click(function ( e ) {
-			e.preventDefault();
+			var span = document.createElement('span');
+			span.appendChild(document.createTextNode(text));
+			h3.appendChild(span);
 
-			if ( !Twinkle.userAuthorized ) {
-				alert("抱歉，您需达自动确认后方可使用Twinkle。");
-			}
-		});
+			var a = document.createElement('a');
+			a.href = '#';
 
-		h5.appendChild( a );
+			$(a).click(function(e) {
+				e.preventDefault();
+			});
+
+			h3.appendChild(a);
+		}
+
+		outerDiv.appendChild(h3);
+		ul.className = 'menu vector-menu-content-list';  // remove menu after 1.35-wmf.37 goes live
 	} else {
-		h5.appendChild( document.createTextNode( text ) );
+		h3.appendChild(document.createTextNode(text));
+		outerDiv.appendChild(h3);
 	}
-	outerDiv.appendChild( h5 );
 
-	var innerDiv = null;
-	if ( type === "menu" ) {
-		innerDiv = document.createElement( "div" );
+	if (innerDivClass) {
+		var innerDiv = document.createElement('div');
 		innerDiv.className = innerDivClass;
+		innerDiv.appendChild(ul);
 		outerDiv.appendChild(innerDiv);
+	} else {
+		outerDiv.appendChild(ul);
 	}
 
-	var ul = document.createElement( "ul" );
-	(innerDiv || outerDiv).appendChild( ul );
 
 	return outerDiv;
 };
